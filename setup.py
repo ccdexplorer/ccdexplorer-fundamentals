0695c2877ec4a273529b66c7c91533cdb2b5abde
Sander de Ruiter <sdr@16april1973.nl>
2024-03-14 10:09:24 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ccdefundamentals",
-    version="0.1.1",
+    version="0.1.2",
     author="Sander de Ruiter",
     author_email="sdr@ccdexplorer.io",
     description="Shared code for CCDExplorer.io and its Notification Bot.",
